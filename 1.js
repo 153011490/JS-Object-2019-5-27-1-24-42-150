5710ec7c2329f17234864897bbb7e42df29aeade
lrh <153011490@qq.com>
2019-06-23 23:52:26 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,9 @@
+var user = {};
+var name = "name";
+var val = "John";
+var surname = "surname";
+var val2 = "Mike";
+user[name] = val; 
+user[surname] = val2;
+user[name]="Peter";
+delete user.name;
